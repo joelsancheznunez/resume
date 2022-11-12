bba2dc73d81edf0b39a769fd9983b2656ab47ed6
joelsancheznunez <joelsanchez.email@gmail.com>
2022-11-11 22:51:33 -0300

    JS and CSS updates

--- main.js
+++ main.js
@@ -23,9 +23,11 @@ function linkAction() {
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction)) 
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 //==================== ACTIVE LINK - SCROLL SECTION ====================//
+/*
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
@@ -45,6 +47,8 @@ function scrollActive() {
     })
 }
 window.addEventListener('scroll', scrollActive)
+*/
+
 
 /*==================== SHOW SCROLL TOP ====================*/ 
 function scrollTop(){
