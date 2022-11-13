9e5c7dcf3bcaf6cae4ce2562c29a27c44e72c796
joelsancheznunez <joelsanchez.email@gmail.com>
2022-11-13 19:54:46 -0300

    Major general updates

--- popup-menu.js
+++ popup-menu.js
@@ -1,17 +1,17 @@
-const optionMenu = document.querySelector(".select-menu"),
-  selectBtn = optionMenu.querySelector(".select-btn"),
-  options = optionMenu.querySelectorAll(".option"),
-  sBtn_text = optionMenu.querySelector(".sBtn-text");
+function popUpMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-selectBtn.addEventListener("click", () =>
-  optionMenu.classList.toggle("active")
-);
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    let selectedOption = option.querySelector(".option-text").innerText;
-    sBtn_text.innerText = selectedOption;
-
-    optionMenu.classList.remove("active");
-  });
-});
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
